b54a76210b655337d562f5c73f8f918d9e5c0e67
SyedAhsanFatmi <ahsan.fatmi21@gmail.com>
2025-11-09 17:34:24 +0500

    clean for next section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import Fetch from "./components/Fetch";
-import Header from "./components/Header";
-import Main from "./components/Main";
 
 export default function App() {
 
     return (
         <>
-            <Header />
-            <Main />
-            <Fetch />
         </>
     )
 }
